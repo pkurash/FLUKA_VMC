91cbcdf4e95827ba48e58719b3e4168131492487
Peter Hristov <Peter.Hristov@cern.ch>
2007-12-18 10:45:22 +0000

    Initial import

---
+++ source/TFlukaLinkDef.h
@@ -0,0 +1,21 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id: TFlukaLinkDef.h,v 1.8 2004/10/06 14:11:55 morsch Exp $ */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+#pragma link C++ class  TFluka+;
+#pragma link C++ class  TFlukaMCGeometry+;
+#pragma link C++ class  TFlukaCerenkov+;
+#pragma link C++ class  TFlukaConfigOption+;
+#pragma link C++ class  TFlukaScoringOption+;
+#endif
+
+
+
+
+
---
+++ source/TFlukaMCLinkDef.h
@@ -0,0 +1,19 @@
+#ifdef __CINT__
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
+ * See cxx source for full Copyright notice                               */
+
+/* $Id: TFlukaMCLinkDef.h,v 1.2 2004/04/14 14:54:30 morsch Exp $ */
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+#pragma link C++ class  TFluka+;
+#pragma link C++ class  TFlukaMCGeometry+;
+#pragma link C++ class  TFlukaCerenkov+;
+#endif
+
+
+
+
+
