3cbdaef8a2a08c8be89d70e5bb84522e0874ee32
Andreas Morsch <Andreas.Morsch@cern.ch>
2012-11-14 16:23:00 +0000

    Correction

--- source/TFlukaMCGeometry.cxx
+++ source/TFlukaMCGeometry.cxx
@@ -1264,7 +1264,7 @@ void  jomiwr(Int_t & nge, const Int_t & /*lin*/, const Int_t & /*lou*/,
 // Geometry initialization wrapper called by FLUKAM. Provides to FLUKA the
 // number of regions (volumes in TGeo)
    // build application geometry
-   nge = 3; // FLUGG returns this...
+  //   nge = 3; // FLUGG returns this...
    if (gMCGeom->IsDebugging()) printf("========== Inside JOMIWR\n");
    flukaReg = gGeoManager->GetListOfUVolumes()->GetEntriesFast()+1;
    if (gMCGeom->IsDebugging()) printf("<= JOMIWR: last region=%i\n", flukaReg);
